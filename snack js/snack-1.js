33f124082e19e67a7fa8267065d60b4b822d0eee
pierfilippodiviccaro <diviccarofilippo@gmail.com>
2025-11-10 14:34:03 +0100

    feat: parzialmente completato l'esercizio 2

--- snack js/snack-1.js
+++ snack js/snack-1.js
@@ -1,25 +1,25 @@
-const biciclette = [
-    {
-        nome: "bicicletta-1",
-        peso: 200,
-    },
-    {
-        nome: "bicicletta-2",
-        peso: 102,
-    },
-    {
-        nome: "bicicletta-3",
-        peso: 80,
-    },
-    {
-        nome: "bicicletta-4",
-        peso: 400
-    }
-]
-for (i = 0; i < biciclette.length; i++) {
-    if (biciclette[i].peso < 100) {
-        console.log(biciclette[i])
+// const biciclette = [
+//     {
+//         nome: "bicicletta-1",
+//         peso: 200,
+//     },
+//     {
+//         nome: "bicicletta-2",
+//         peso: 102,
+//     },
+//     {
+//         nome: "bicicletta-3",
+//         peso: 80,
+//     },
+//     {
+//         nome: "bicicletta-4",
+//         peso: 400
+//     }
+// ]
+// for (i = 0; i < biciclette.length; i++) {
+//     if (biciclette[i].peso < 100) {
+//         console.log(biciclette[i])
 
 
-    }
-}
+//     }
+// }
---
+++ snack js/snack-2.js
@@ -0,0 +1,38 @@
+//creazione dell'array
+
+
+const squadre = [
+    {
+        nome: "miaofc",
+        gol: 0,
+        falli: 0,
+    },
+    {
+        nome: "milan",
+        gol: 0,
+        falli: 0,
+
+    },
+    {
+        nome: "inter",
+        gol: 0,
+        falli: 0
+    },
+    {
+        nome: "bayern",
+        gol: 0,
+        falli: 0,
+    }
+
+]
+
+
+//operazioni di logica (prima parte dell'esercizio)
+for (i = 0; i < squadre.length; i++) {
+    const squadra = squadre[i]
+    squadra.gol = Math.floor(Math.random() * 5)
+    squadra.falli = Math.floor(Math.random() * 3)
+    const falliSubiti=[]
+    
+
+}
