9f39fa4957f2552fc37a76d12f467b5e2b3f0071
pierfilippodiviccaro <diviccarofilippo@gmail.com>
2025-11-10 15:46:48 +0100

    feat: esercizio 2 sistemato

--- snack js/snack-1.js
+++ snack js/snack-1.js
@@ -1,25 +1,24 @@
-// const biciclette = [
-//     {
-//         nome: "bicicletta-1",
-//         peso: 200,
-//     },
-//     {
-//         nome: "bicicletta-2",
-//         peso: 102,
-//     },
-//     {
-//         nome: "bicicletta-3",
-//         peso: 80,
-//     },
-//     {
-//         nome: "bicicletta-4",
-//         peso: 400
-//     }
-// ]
-// for (i = 0; i < biciclette.length; i++) {
-//     if (biciclette[i].peso < 100) {
-//         console.log(biciclette[i])
 
+const biciclette = [
+    {
+        nome: "bicicletta-1",
+        peso: 200,
+    },
+    {
+        nome: "bicicletta-2",
+        peso: 102,
+    },
+    {
+        nome: "bicicletta-3",
+        peso: 80,
+    },
+    {
+        nome: "bicicletta-4",
+        peso: 400
+    }
+]
+const pesi = biciclette.map(bici => bici.peso); //arrow function
+const pesoMinimo = Math.min(...pesi); //utilizzo dello spread operator
+const indicePesoMinimo = pesi.indexOf(pesoMinimo);
+console.log(biciclette[indicePesoMinimo]);
 
-//     }
-// }
--- snack js/snack-2.js
+++ snack js/snack-2.js
@@ -1,43 +1,43 @@
 //creazione dell'array
 
 
-const squadre = [
-    {
-        nome: "miaofc",
-        gol: 0,
-        falli: 0,
-    },
-    {
-        nome: "milan",
-        gol: 0,
-        falli: 0,
+// const squadre = [
+//     {
+//         nome: "miaofc",
+//         gol: 0,
+//         falli: 0,
+//     },
+//     {
+//         nome: "milan",
+//         gol: 0,
+//         falli: 0,
 
-    },
-    {
-        nome: "inter",
-        gol: 0,
-        falli: 0
-    },
-    {
-        nome: "bayern",
-        gol: 0,
-        falli: 0,
-    }
+//     },
+//     {
+//         nome: "inter",
+//         gol: 0,
+//         falli: 0
+//     },
+//     {
+//         nome: "bayern",
+//         gol: 0,
+//         falli: 0,
+//     }
 
-]
+// ]
 
 
 //operazioni di logica 
-for (i = 0; i < squadre.length; i++) {
-    const squadra = squadre[i]
-    squadra.gol = Math.floor(Math.random() * 5)
-    squadra.falli = Math.floor(Math.random() * 5)
-}
-const squadreFalli = []
-for(i=0;i<squadre.length;i++){
-    squadreFalli.push({
-        nome:squadre[i].nome,
-        falli:squadre[i].falli,
-    })
-}
-console.log(squadreFalli);
+// for (i = 0; i < squadre.length; i++) {
+//     const squadra = squadre[i]
+//     squadra.gol = Math.floor(Math.random() * 5)
+//     squadra.falli = Math.floor(Math.random() * 5)
+// }
+// const squadreFalli = []
+// for(i=0;i<squadre.length;i++){
+//     squadreFalli.push({
+//         nome:squadre[i].nome,
+//         falli:squadre[i].falli,
+//     })
+// }
+// console.log(squadreFalli);
