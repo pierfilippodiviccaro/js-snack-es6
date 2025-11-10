64045f1830718df941f0e24d84c1a48307336504
pierfilippodiviccaro <diviccarofilippo@gmail.com>
2025-11-10 14:13:44 +0100

    first commit

---
+++ snack js/snack-1.js
@@ -0,0 +1,26 @@
+const biciclette = [
+    {
+        nome: "bicicletta-1",
+        peso: 200,
+    },
+    {
+        nome: "bicicletta-2",
+        peso: 102,
+    },
+    {
+        nome: "bicicletta-3",
+        peso: 80,
+    },
+    {
+        nome: "bicicletta-4",
+        peso: 400
+    }
+]
+for (i = 0; i < biciclette.length; i++) {
+    if (biciclette[i].peso < 100) {
+        console.log(biciclette[i])
+
+
+    }
+
+}
